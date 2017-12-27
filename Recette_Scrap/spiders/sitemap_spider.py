eca362202c1b93845ef21c2506a15c6d6a0af1e2
johan-gras <johan.gras@u-psud.fr>
2017-12-27 22:09:36 +0100

    Bug Correction
    Corrections de bugs où une entrée est créer dans une recette alors qu'il
    n'y à pas de données

--- Recette_Scrap/spiders/sitemap_spider.py
+++ Recette_Scrap/spiders/sitemap_spider.py
@@ -1,7 +1,8 @@
 from scrapy.spiders import SitemapSpider
 
 class MySpider(SitemapSpider):
-    name = "scrap_sitemap" 
+    name = "scrap_sitemap"
+    #Use of the sitemap module
     sitemap_urls = ['http://www.marmiton.org/wsitemap_1.xml']
     sitemap_rules = [
         ('/recettes/', 'parse_recettes'),
@@ -9,35 +10,37 @@ class MySpider(SitemapSpider):
 
     def parse_recettes(self, response):
         #Time parsing
-        time = {
-            'total' : response.css(".title-2.recipe-infos__total-time__value::text").extract_first(),
-            'preparation' : response.css(".recipe-infos__timmings__preparation .recipe-infos__timmings__value::text").extract_first().strip("\r\n\t"),
-        }
-        cooking = response.css(".recipe-infos__timmings__cooking .recipe-infos__timmings__value::text").extract_first()
-        if cooking is not None:
-            time['cooking'] = cooking.strip("\r\n\t")
+        time = {}
+        total = ('total', response.css(".title-2.recipe-infos__total-time__value::text").extract_first())
+        preparation = ('preparation', response.css(".recipe-infos__timmings__preparation .recipe-infos__timmings__value::text").extract_first())
+        cooking = ('cooking', response.css(".recipe-infos__timmings__cooking .recipe-infos__timmings__value::text").extract_first())
+        safe_insert(total, time)
+        safe_insert(preparation, time)
+        safe_insert(cooking, time)
         
         #Ingredients parsing
-        ingredients_selector = response.css(".recipe-ingredients__list__item")
         ingredients = []
+        ingredients_selector = response.css(".recipe-ingredients__list__item")
         for ingredient in ingredients_selector:
-            dict = {'name' : ingredient.css(".ingredient::text").extract_first()}
-            quantity = ingredient.css(".recipe-ingredient-qt::text").extract_first()
-            if quantity is not None:
-                dict['quantity'] = quantity
-            ingredients.append(dict)
+            dict_ingredient = {}
+            name = ('name', ingredient.css(".ingredient::text").extract_first())
+            quantity = ('quantity', ingredient.css(".recipe-ingredient-qt::text").extract_first())
+            safe_insert(name, dict_ingredient)
+            safe_insert(quantity, dict_ingredient)
+
+            ingredients.append(dict_ingredient)
         
         #Utensils parsing
-        utensils_selector = response.css(".recipe-utensil__name::text").extract()
         utensils = []
+        utensils_selector = response.css(".recipe-utensil__name::text").extract()
         for utensil in utensils_selector:
             ustensil = utensil.strip("\r\n\t")
             if ustensil != "":
                 utensils.append(ustensil)
         
         #Etapes parsing
-        etapes_selector = response.css(".recipe-preparation__list__item")
         etapes = []
+        etapes_selector = response.css(".recipe-preparation__list__item")
         for i, etape in enumerate(etapes_selector):
             description = ""
             for string in etape.css("::text").extract():
@@ -50,24 +53,32 @@ class MySpider(SitemapSpider):
             etapes.append(dict)
         
         #Main parsing
-        scraped_recipe = {
-            'url' : response.url,
-            'title' : response.css("h1.main-title::text").extract_first(),
-            
-            'time' : time,
-            'number_of_person' : response.css(".title-2.recipe-infos__quantity__value::text").extract_first(),
-            'level' :  response.css(".recipe-infos__level").xpath("div/@class").re_first(".$"),
-            'budget' :  response.css(".recipe-infos__budget").xpath("div/@class").re_first(".$"),
-            
-            'ingredients' : ingredients,
-            'utensils' : utensils,
-            'etapes' : etapes
-        }
+        scraped_recipe = {'url' : response.url}
         
-        #Rating scraping
-        rating = response.css(".recipe-reviews-list__review__head__infos__rating__value::text").extract_first()
-        if rating is not None:
-            scraped_recipe['rating'] = rating
+        title = ('title', response.css("h1.main-title::text").extract_first())
+        number_of_person = ('number_of_person', response.css(".title-2.recipe-infos__quantity__value::text").extract_first())
+        rating = ('rating', response.css(".recipe-reviews-list__review__head__infos__rating__value::text").extract_first())
+        level = ('level', response.css(".recipe-infos__level").xpath("div/@class").re_first(".$"))
+        budget = ('budget', response.css(".recipe-infos__budget").xpath("div/@class").re_first(".$"))
+
+        safe_insert(title, scraped_recipe)
+        safe_insert(number_of_person, scraped_recipe)
+        safe_insert(rating, scraped_recipe)
+        safe_insert(level, scraped_recipe)
+        safe_insert(budget, scraped_recipe)
+        safe_insert(('time', time), scraped_recipe)
+        safe_insert(('ingredients', ingredients), scraped_recipe)
+        safe_insert(('utensils', utensils), scraped_recipe)
+        safe_insert(('etapes', etapes), scraped_recipe)
         
         yield scraped_recipe
-        
+    
+#Insert in the dict valid data
+def safe_insert(data, dictio):
+    (name, value) = data
+    if isinstance(value, str) and value is not None:
+        dictio[name] = value.strip("\r\n\t")
+    elif isinstance(value, list) and value:
+        dictio[name] = value
+    elif isinstance(value, dict) and value:
+        dictio[name] = value
