c6c455eeb30eba5bdb87a0ea6b095939556efe83
johan.gras <johan.gras-sub@outlook.com>
2018-01-31 10:07:01 +0100

    UPDATE/ADD
    Database API Prototype

--- Recipe_Player/Database_Creator.py
+++ Recipe_Player/Database_Creator.py
@@ -10,10 +10,12 @@ class Database:
     password = ""
     database_name = "big_cooking_data"
 
-    # Open database connection
-    db = MySQLdb.connect(location, user, password, database_name)
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
+    db = None
+    cursor = None
+
+    def connect(self):
+        self.db = MySQLdb.connect(self.location, self.user, self.password, self.database_name)
+        self.cursor = self.db.cursor()
 
     def disconnect(self):
         self.db.close()
@@ -21,3 +23,18 @@ class Database:
     def get_version(self):
         self.cursor.execute("SELECT VERSION()")
         return self.cursor.fetchone()[0]
+
+    def create_db(self):
+        self.connect()
+        #
+        # TO DO : TABLES DROP AND CREATION
+        #
+        self.disconnect()
+
+    def build_db(self, json):
+        self.connect()
+        #
+        # TO DO : FOR ALL RECIPES DO THE INSERTS IN TABLES
+        #         USING SUB-FUNCTION FOR EACH TYPES OF INSERTS IS A GOOD WAY IMO
+        #
+        self.disconnect()
