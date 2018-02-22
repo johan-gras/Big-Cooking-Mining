8aea82133efe05d1466dc81cbb64a5ef2bdc6b45
MaxenceLagmairi <lagmairi.maxence@gmail.com>
2018-02-22 14:04:58 +0100

    [UPDATE]
    le fichier ne fait pas encore l'insert ^^ mais juste pour que vous aillez le fichier maj, je continue

--- Recipe_Player/Database_Creator.py
+++ Recipe_Player/Database_Creator.py
@@ -1,11 +1,9 @@
-#If MySQLdb not installed
-
-#pip install mysqlclient
+# If MySQLdb not installed
+# pip install mysqlclient
 
 import MySQLdb
 import pandas as pd
-from  more_itertools import unique_everseen
-
+#from more_itertools import unique_everseen
 
 '''
 class Recipe(IsDescription):
@@ -22,11 +20,7 @@ class Recipe(IsDescription):
 '''
 
 
-
-
-
 class Database:
-
     # Database arguments :
 
     location = "localhost"
@@ -35,16 +29,11 @@ class Database:
 
     password = ""
 
-    database_name = "bigcookingdata"
+    database_name = "big_cooking_data"
 
     db = None
 
     cursor = None
-
-## Json file Reader
-    #recipes = JsonReader()
-    file_rec = 'test.json'
-
 
 
     def connect(self):
@@ -53,13 +42,9 @@ class Database:
 
         self.cursor = self.db.cursor()
 
-
-
     def disconnect(self):
 
         self.db.close()
-
-
 
     def get_version(self):
 
@@ -67,37 +52,39 @@ class Database:
 
         return self.cursor.fetchone()[0]
 
-
-
     def create_db(self):
 
         self.connect()
-
 
         sql = """
                 DROP TABLE IF EXISTS RECIPE;
                 DROP TABLE IF EXISTS INGREDIENT;
                 DROP TABLE IF EXISTS USER_PROFILE;
                 DROP TABLE IF EXISTS UTENSIL;
-                DROP TABLE IF EXISTS RECIPE_STEP;
+                DROP TABLE IF EXISTS STEP;
                 DROP TABLE IF EXISTS SPORT_ACTIVITY;
-                       
-        
-        
+                DROP TABLE IF EXISTS CATEGORIE;
+                DROP TABLE IF EXISTS L_RECIPE_STEP;
+                DROP TABLE IF EXISTS L_RECIPE_INGREDIENT;
+
                 CREATE TABLE RECIPE (
                  ID_RECIPE INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                 TITLE_RECIPE  VARCHAR(200),
+                 TITLE_RECIPE VARCHAR(200),
                  LEVEL_RECIPE VARCHAR(100),  
                  NUMBER_OF_PERSON VARCHAR(50),
                  RATING_RECIPE VARCHAR(100),
                  TIME_TOTAL VARCHAR(100),
                  TIME_PREPA VARCHAR(100),
-                 TIME_COOKING VARCHAR(100));
-                 
+                 TIME_COOKING VARCHAR(100),
+                 BUDGET VARCHAR(20));
+
                  CREATE TABLE INGREDIENT (
                  ID_INGREDIENT INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                 NAME_INGREDIENT VARCHAR(100));
-                 
+                 NAME_INGREDIENT VARCHAR(100),
+                 QUANTITY_INGREDIENT VARCHAR(20),
+                 TITLE_RECIPE VARCHAR(255),
+                 ING_ID VARCHAR(20));
+
                  CREATE TABLE USER_PROFILE (
                  ID_USER INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  USERNAME VARCHAR(50),
@@ -106,17 +93,29 @@ class Database:
                  WEIGHT_USER VARCHAR(50),
                  HEIGHT_USEER VARCHAR(50),
                  REGISTRATION_DATE DATETIME);
-                 
+
                  CREATE TABLE UTENSIL (
-                 ID_UTENSIL INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  NAME_UTENSIL VARCHAR(100),
-                 QUANTITY VARCHAR(20));
-                 
-                 CREATE TABLE RECIPE_STEP (
+                 TITLE_RECIPE VARCHAR(255));
+
+                 CREATE TABLE STEP (
                  ID_STEP INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                 STPE_NUMBER VARCHAR(50),
+                 STEP_NUMBER VARCHAR(50),
                  DESCRIPTION_STEP VARCHAR(255));
                  
+                 CREATE TABLE CATEGORIE (
+                 ID_CATEGORIE INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                 CATEGORIE_NAME VARCHAR(255),
+                 TITLE_RECIPE VARCHAR(255));
+                 
+                 CREATE TABLE L_RECIPE_STEP (
+                 TITLE_RECIPE VARCHAR(255),
+                 DESCRIPTION_STEP VARCHAR(255));
+                 
+                 CREATE TABLE L_RECIPE_INGREDIENT (
+                 TITLE_RECIPE VARCHAR(255),
+                 NAME_INGREDIENT VARCHAR(255));
+
                  CREATE TABLE SPORT_ACTIVITY (
                  ID_ACTIVITY INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  NAME_ACTIVITY VARCHAR(100),
@@ -129,54 +128,150 @@ class Database:
 
         print("La Base de données a bien été créée")
 
+    def build_db(self, recipes):
 
-
-
-    def build_db(self,file_rec):
-
+        global sql_recipe, sql_step, sql_ingredient, sql_utensils, liste_ingre_totale_pd
         compteur = 0
-        for values in file_rec.recipes:
+        liste_ingre_totale = []
+        liste_ingre_by_recipe = []
+        self.connect()
+        for values in recipes.recipes:
 
-            self.connect()
-
-            compteur = compteur + 1
-            print("Boucle For ",compteur,values)
-
-
-            title = values.get('title')
-            level = values.get('level')
+            steps = values.get('etapes')
             number_of_person = values.get('number_of_person')
+            time = values.get('time')
             rating = values.get('rating')
-            time_total = values.get('time').get('total')
-            time_prepa = values.get('time').get('preparation')
-            time_cooking = values.get('time').get('cooking')
+            level = values.get('level')
+            categories = values.get('categories')
+            ingredients = values.get('ingredients')
+            budget = values.get('budget')
+            title_recipe = values.get('title')
+            utensils = values.get('utensils')
+            time_total = 0
+            time_prepa = 0
+            time_cooking = 0
 
+            #Condition if None everywhere
+            if title_recipe and level and number_of_person and rating and budget and steps and time_total and time_prepa and time_cooking is None:
+                pass
+            # Affichage numerous recipe et compteur
+            compteur = compteur + 1
+            print("--- NUMEROUS RECIPE :", compteur, ":", title_recipe, "---")
 
+            # Table Recipe
+            # Condition if Budget is None
+            if budget is None:
+                budget = 0
+                sql_recipe = """insert into recipe (title_recipe,level,number_of_person,rating,time_total,time_prepa,time_cooking,budget)\
+                    values ('%s','%s','%s','%s','%s','%s','%s','%s','%s');""" % (
+                title_recipe, level, number_of_person, rating, time_total, time_prepa, time_cooking, budget, categories)
+            # Condition if Time is None
+            elif time is None:
+                sql_recipe = """insert into recipe (title_recipe,level,number_of_person,rating,time_total,time_prepa,time_cooking,budget)\
+                    values ('%s','%s','%s','%s','%s','%s','%s','%s','%s');""" % (
+                title_recipe, level, number_of_person, rating, 0, 0, 0, budget, categories)
 
-            sql = """insert into recipe (title,level,number_of_person,rating,time_total,time_prepa,time_cooking) \
-               VALUES ('%s','%s','%s','%s','%s','%s','%s');""" % (title,level,number_of_person,rating,time_total,time_prepa,time_cooking)
+            # Time == full
+            else:
+                time_total = values.get('time').get('total')
+                time_prepa = values.get('time').get('preparation')
+                time_cooking = values.get('time').get('cooking')
+                sql_recipe = """insert into recipe (title_recipe,level,number_of_person,rating,time_total,time_prepa,time_cooking,budget)\
+                    values ('%s','%s','%s','%s','%s','%s','%s','%s','%s');""" % (
+                title_recipe, level, number_of_person, rating, time_total, time_prepa, time_cooking, budget, categories)
+            print("RECIPE ::", sql_recipe)
 
+            # Table step
+            if steps is None:
+                pass
+                print("Pas d'étape")
+            else:
+                for step in steps:
+                    sql_step = ""
+                    step_num = step['Etape']
+                    step_desc = step['Description']
+                    sql_step = """insert into step (step_number,desc_step, title_recipe) values ('%s','%s','%s');""" % (
+                    step_num, step_desc, title_recipe)
+                    print("STEP ::", sql_step)
+
+            # Table ingredient
+            if ingredients is None:
+                pass
+                print("0 ingrédients")
+            else:
+                for ing in ingredients:
+                    liste_ingre = []
+                    liste_ingre_by_recipe = []
+                    ing_id = ing.get('id')
+                    ing_quantity = ing.get('quantity')
+                    ing_name = ing.get('name')
+                    liste_ingre.append(ing_name)
+
+                    if ing_quantity is None:
+                        sql_ingredient = """ insert into ingredient (name_ingredient,quantity_ingredient,title_recipe,ing_id) values('%s','%s','%s','%s');""" % (
+                        ing_name, None, title_recipe, ing_id)
+                    else:
+                        sql_ingredient = """ insert into ingredient (name_ingredient,quantity_ingredient,title_recipe,ing_id) values('%s','%s','%s','%s');""" % (
+                        ing_name, ing_quantity, title_recipe, ing_id)
+                    # import ipdb;ipdb.set_trace()
+                    print("INGREDIENT :", sql_ingredient)
+
+                    liste_ingre_by_recipe.extend(liste_ingre)
+                liste_ingre_totale.extend((liste_ingre_by_recipe))
+                liste_ingre_totale_pd = pd.Series(liste_ingre_totale).drop_duplicates().tolist()
+
+            # Table Utensils
+            if utensils is None:
+                pass
+                print("0 utensils")
+            else:
+                for utensil in utensils:
+                    sql_utensils = """ insert into utensil (name_utensil,title_recipe) values ('%s','%s');""" % (
+                    utensil, title_recipe)
+                    print("UTENSIL ::", sql_utensils)
+
+            # Injection des données en base
             try:
-            #Execute the SQL command
-                self.cursor.execute(sql)
-                    # Commit your changes in the database
+                # Execute the SQL command
+                self.cursor.execute(sql_recipe)
+                # self.cursor.execute(sql_step)
+                # self.cursor.execute(sql_utensils)
+                # self.cursor.execute(sql_ingredient)
+                # Commit your changes in the database
                 self.db.commit()
             except:
                 # Rollback in case there is any error
                 self.db.rollback()
 
-            self.disconnect()
+        self.disconnect()
 
+        print("Nombre d'ingrédients : ", len(liste_ingre_totale_pd))
         print("La Base de données a bien été importée")
 
-    def truncate_table_recipe(self):
+    def truncate_db(self):
         self.connect()
 
-        sql = "truncate table recipe;"
-
+        sql_recipe = "truncate table recipe;"
+        sql_utensil = "truncate table utensil;"
+        sql_ingre = "truncate table ingredient;"
+        sql_step = "truncate table step;"
+        sql_ingredient = "truncate table ingredient;"
+        sql_categorie = "truncate table categorie;"
+        sql_l_recipe_step = "truncate table l_recipe_step;"
+        sql_l_recipe_ingredient = "truncate table l_recipe_ingredient;"
+        sql_sport_activity = "truncate table sport_activity;"
         try:
             # Execute the SQL command
-            self.cursor.execute(sql)
+            self.cursor.execute(sql_recipe)
+            self.cursor.execute(sql_utensil)
+            self.cursor.execute(sql_ingre)
+            self.cursor.execute(sql_step)
+            self.cursor.execute(sql_ingredient)
+            self.cursor.execute(sql_categorie)
+            self.cursor.execute(sql_l_recipe_step)
+            self.cursor.execute(sql_l_recipe_ingredient)
+            self.cursor.execute(sql_sport_activity)
+
             # Commit your changes in the database
             self.db.commit()
         except:
@@ -185,56 +280,4 @@ class Database:
 
         self.disconnect()
 
-        print("La table recipe est bien vidée")
-
-    def build_table_ingredient(self,file_rec):
-
-
-        compteur = 0
-        liste_ingre_totale = []
-        liste_ingre_by_recipe = []
-
-        self.connect()
-
-        for values in file_rec.recipes:
-
-            liste_ingre = []
-            liste_ingre_by_recipe = []
-            compteur = compteur + 1
-            print("***** Contador recetas : ", compteur,"*****")
-            ingredients = values.get('ingredients')
-            title = values.get('title')
-
-
-            for ing in ingredients:
-                #print(ing['name'])
-                ing_name=ing['name']
-                liste_ingre.append(ing_name)
-
-                sql = """ insert into ingredient (name_ingredient) values('%s');""" % (ing_name)
-                try:
-                    # Execute the SQL command
-                    #self.cursor.execute(sql)
-                    # Commit your changes in the database
-                    self.db.commit()
-                except:
-                    # Rollback in case there is any error
-                    self.db.rollback()
-
-            liste_ingre_by_recipe.extend(liste_ingre)
-            print("___Nombre d'ingredients pour la recette", title, ":", len(liste_ingre_by_recipe))
-            print("___Ingrédients :",liste_ingre_by_recipe)
-
-            liste_ingre_totale.extend((liste_ingre_by_recipe))
-            #[liste_ingre_totale.extend(i) for i in liste_ingre_by_recipe if not i in liste_ingre_totale]
-            #print("Liste ingre everseen totale sans doublons",len(list(unique_everseen(liste_ingre_totale))))
-            liste_ingre_totale_pd = pd.Series(liste_ingre_totale).drop_duplicates().tolist()
-            print("Liste ingre  panda sans doublons", len(liste_ingre_totale_pd))
-            print("-----Ingrédients totaux pour toute la base :", liste_ingre_totale_pd,"-----")
-
-        self.disconnect()
-
-
-
-
-
+        print("Database bcd is empty")
