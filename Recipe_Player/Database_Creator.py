1d5ed8220bc77f6d357ef6b0d426c8de7725f963
johan.gras <johan.gras-sub@outlook.com>
2018-05-21 16:07:57 +0200

    Big Big Commit
    Add modules : clustering, recommander, stats.
    And lots of modifs.

--- Recipe_Player/Database_Creator.py
+++ Recipe_Player/Database_Creator.py
@@ -1,11 +1,9 @@
-#If MySQLdb not installed
+# If MySQLdb not installed
 
-#pip install mysqlclient
+# pip install mysqlclient
 
 import MySQLdb
 import pandas as pd
-from  more_itertools import unique_everseen
-
 
 '''
 class Recipe(IsDescription):
@@ -22,11 +20,7 @@ class Recipe(IsDescription):
 '''
 
 
-
-
-
 class Database:
-
     # Database arguments :
 
     location = "localhost"
@@ -41,11 +35,9 @@ class Database:
 
     cursor = None
 
-## Json file Reader
-    #recipes = JsonReader()
-    file_rec = 'test.json'
-
-
+    ## Json file Reader
+    # recipes = JsonReader()
+    file_rec = 'new.json'
 
     def connect(self):
 
@@ -53,13 +45,9 @@ class Database:
 
         self.cursor = self.db.cursor()
 
-
-
     def disconnect(self):
 
         self.db.close()
-
-
 
     def get_version(self):
 
@@ -67,12 +55,9 @@ class Database:
 
         return self.cursor.fetchone()[0]
 
-
-
     def create_db(self):
 
         self.connect()
-
 
         sql = """
                 DROP TABLE IF EXISTS RECIPE;
@@ -81,9 +66,9 @@ class Database:
                 DROP TABLE IF EXISTS UTENSIL;
                 DROP TABLE IF EXISTS RECIPE_STEP;
                 DROP TABLE IF EXISTS SPORT_ACTIVITY;
-                       
-        
-        
+
+
+
                 CREATE TABLE RECIPE (
                  ID_RECIPE INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
                  TITLE_RECIPE  VARCHAR(200),
@@ -93,11 +78,11 @@ class Database:
                  TIME_TOTAL VARCHAR(100),
                  TIME_PREPA VARCHAR(100),
                  TIME_COOKING VARCHAR(100));
-                 
+
                  CREATE TABLE INGREDIENT (
                  ID_INGREDIENT INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  NAME_INGREDIENT VARCHAR(100));
-                 
+
                  CREATE TABLE USER_PROFILE (
                  ID_USER INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  USERNAME VARCHAR(50),
@@ -106,17 +91,17 @@ class Database:
                  WEIGHT_USER VARCHAR(50),
                  HEIGHT_USEER VARCHAR(50),
                  REGISTRATION_DATE DATETIME);
-                 
+
                  CREATE TABLE UTENSIL (
                  ID_UTENSIL INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  NAME_UTENSIL VARCHAR(100),
                  QUANTITY VARCHAR(20));
-                 
+
                  CREATE TABLE RECIPE_STEP (
                  ID_STEP INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  STPE_NUMBER VARCHAR(50),
                  DESCRIPTION_STEP VARCHAR(255));
-                 
+
                  CREATE TABLE SPORT_ACTIVITY (
                  ID_ACTIVITY INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
                  NAME_ACTIVITY VARCHAR(100),
@@ -129,43 +114,79 @@ class Database:
 
         print("La Base de données a bien été créée")
 
-
-
-
-    def build_db(self,file_rec):
+    def build_db(self, file_rec):
 
         compteur = 0
+        self.connect()
+        liste_ingre_totale = []
+        liste_ingre_by_recipe = []
+
         for values in file_rec.recipes:
 
-            self.connect()
-
             compteur = compteur + 1
-            print("Boucle For ",compteur,values)
+            print("Boucle For ", compteur)
 
-
-            title = values.get('title')
-            level = values.get('level')
+            steps = values.get('etapes')
             number_of_person = values.get('number_of_person')
-            rating = values.get('rating')
             time_total = values.get('time').get('total')
             time_prepa = values.get('time').get('preparation')
             time_cooking = values.get('time').get('cooking')
+            rating = values.get('rating')
+            level = values.get('level')
+            category = values.get('category')
+            ingredients = values.get('ingredients')
+            budget = values.get('budget')
+            title_recipe = values.get('title')
 
+            print(title_recipe)
+            for step in steps:
+                step_num = step['Etape']
+                step_desc = step['Description']
+                # title_r = title_recipe
 
+                sql_step = """insert into recipe_step (step_number,desc_step, title_recipe) values ('%s','%s','%s');""" % (
+                step_num, step_desc, title_recipe)
+                print(sql_step)
 
-            sql = """insert into recipe (title,level,number_of_person,rating,time_total,time_prepa,time_cooking) \
-               VALUES ('%s','%s','%s','%s','%s','%s','%s');""" % (title,level,number_of_person,rating,time_total,time_prepa,time_cooking)
+            print(title_recipe)
+            for ing in ingredients:
+                liste_ingre = []
+                liste_ingre_by_recipe = []
+                ing_d = ing.get('id')
+                ing_quantity = ing.get('quantity')
+                ing_name = ing['name']
+                liste_ingre.append(ing_name)
+
+                sql_ingredient = """ insert into ingredient (name_ingredient) values('%s','%s','%s');""" % (
+                ing_name, ing_quantity, title_recipe)
+                # print(ing_quantity)
+                print(sql_ingredient)
+
+            liste_ingre_by_recipe.extend(liste_ingre)
+            liste_ingre_totale.extend((liste_ingre_by_recipe))
+            liste_ingre_totale_pd = pd.Series(liste_ingre_totale).drop_duplicates().tolist()
+            # print("Liste ingredient panda sans doublons", len(liste_ingre_totale_pd))
+
+            ######### Injection des données en base
+
+            sql_recipe = """insert into recipe (title,level,number_of_person,rating,time_total,time_prepa,time_cooking)\
+            VALUES ('%s','%s','%s','%s','%s','%s','%s');""" % (
+            title_recipe, level, number_of_person, rating, time_total, time_prepa, time_cooking)
+
+            print(sql_recipe)
 
             try:
-            #Execute the SQL command
-                self.cursor.execute(sql)
-                    # Commit your changes in the database
+                # Execute the SQL command
+                # self.cursor.execute(sql_recipe)
+                # self.cursor.execute(sql_step)
+                # self.cursor.execute(sql_ingredient)
+                # Commit your changes in the database
                 self.db.commit()
             except:
                 # Rollback in case there is any error
                 self.db.rollback()
 
-            self.disconnect()
+        self.disconnect()
 
         print("La Base de données a bien été importée")
 
@@ -187,8 +208,7 @@ class Database:
 
         print("La table recipe est bien vidée")
 
-    def build_table_ingredient(self,file_rec):
-
+    def build_table_ingredient(self, file_rec):
 
         compteur = 0
         liste_ingre_totale = []
@@ -201,20 +221,19 @@ class Database:
             liste_ingre = []
             liste_ingre_by_recipe = []
             compteur = compteur + 1
-            print("***** Contador recetas : ", compteur,"*****")
+            print("***** Contador recetas : ", compteur, "*****")
             ingredients = values.get('ingredients')
             title = values.get('title')
 
-
             for ing in ingredients:
-                #print(ing['name'])
-                ing_name=ing['name']
+                # print(ing['name'])
+                ing_name = ing['name']
                 liste_ingre.append(ing_name)
 
                 sql = """ insert into ingredient (name_ingredient) values('%s');""" % (ing_name)
                 try:
                     # Execute the SQL command
-                    #self.cursor.execute(sql)
+                    # self.cursor.execute(sql)
                     # Commit your changes in the database
                     self.db.commit()
                 except:
@@ -223,14 +242,14 @@ class Database:
 
             liste_ingre_by_recipe.extend(liste_ingre)
             print("___Nombre d'ingredients pour la recette", title, ":", len(liste_ingre_by_recipe))
-            print("___Ingrédients :",liste_ingre_by_recipe)
+            print("___Ingrédients :", liste_ingre_by_recipe)
 
             liste_ingre_totale.extend((liste_ingre_by_recipe))
-            #[liste_ingre_totale.extend(i) for i in liste_ingre_by_recipe if not i in liste_ingre_totale]
-            #print("Liste ingre everseen totale sans doublons",len(list(unique_everseen(liste_ingre_totale))))
+            # [liste_ingre_totale.extend(i) for i in liste_ingre_by_recipe if not i in liste_ingre_totale]
+            # print("Liste ingre everseen totale sans doublons",len(list(unique_everseen(liste_ingre_totale))))
             liste_ingre_totale_pd = pd.Series(liste_ingre_totale).drop_duplicates().tolist()
             print("Liste ingre  panda sans doublons", len(liste_ingre_totale_pd))
-            print("-----Ingrédients totaux pour toute la base :", liste_ingre_totale_pd,"-----")
+            print("-----Ingrédients totaux pour toute la base :", liste_ingre_totale_pd, "-----")
 
         self.disconnect()
 
--- Recipe_Player/Recipe_Player.py
+++ Recipe_Player/Recipe_Player.py
@@ -1,118 +1,91 @@
-
-
 from JsonReader import JsonReader
 from Database_Creator import Database
-
-
-
+from Stats import Stats
+from Prototype import Prototype
+from Clustering import Clustering
 
 close = False
-
 json = JsonReader()
-
 db = Database()
-
-
-
+stats = Stats(json)
+recommender = Prototype(json)
+clustering = Clustering(json)
 
 
 def user_command():
-
     global close
-
     command = input()
 
-
-
     # Id recipe
-
     if command.isdigit():
-
         json.id = int(command)
-
         json.read_recipe()
-
-
 
     # Next recipe
-
     elif not command:
-
         json.id += 1
-
         json.read_recipe()
 
-
-
     # Create/recreate database
-
     elif command == "create_db":
-
         db.create_db()
 
-
-
     # Create/recreate database
-
     elif command == "build_db":
-
         db.build_db(json)
 
-
     # Vider la table recipe
-
     elif command == "truncate_table_recipe":
-
         db.truncate_table_recipe()
 
     elif command == "build_table_ingredient":
-
         db.build_table_ingredient(json)
 
+    # Appel au module stats
+    elif command.find('stats ') != -1:
+        command = command.replace('stats ', '')
+        getattr(stats, command)()
+
+    elif command.find('stats') != -1:
+        stats.helper()
+
+    # Appel au module prototype
+    elif command.find('recommender ') != -1:
+        command = command.replace('recommender ', '')
+        getattr(recommender, command)()
+
+    elif command.find('recommender') != -1:
+        recommender.helper()
+
+    # Appel au module cluster
+    elif command.find('cluster ') != -1:
+        command = command.replace('cluster ', '')
+        getattr(clustering, command)()
+
     # Exit
-
     elif command == 'exit':
-
         close = True
 
-
-
     else:
-
         print("Unknow command !\n")
-
-
-
 
 
 def main():
 
     print('Fichier ouvert avec ' + str(len(json.recipes)) + ' recettes\n')
-
     print('Taper un id de recette')
-
-    print('Appuyer sur entré pour les faire défilé')
-
+    print('Appuyer sur entré pour les faire défilé\n')
     print('"create_db" pour créer ou recrée la base')
-
     print('"build_db" pour remplir la base avec les recettes')
-
     print('"truncate_table_recipe" pour vider la table recipe')
-
     print('"build_table_ingredient" pour remplir la base avec les ingredient')
-
+    print('"stats" pour le module de statistique')
+    print('"recommender" pour le module de recommendation')
     print('"exit" pour fermé')
 
-
-
     while not close:
-
         print()
-
         user_command()
-
-
-
 
 
 main()
